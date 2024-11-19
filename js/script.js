4c09edfb7f247c7c7ce0c024c1ef6a2093a0811e
bondarin87 <110008441+bondarin87@users.noreply.github.com>
2024-11-19 09:59:14 -0500

    Actualizar script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let btn = document.querySelector('#btn');
 
 btn.addEventListener('click', function func() {
     for (let i=0; i<elems.length; i++) {
-        elems[i].textContent += (i+1);
+        elems[i].textContent = elems[i].textContent + (i+1);
     }
     btn.removeEventListener('click', func)
 }
